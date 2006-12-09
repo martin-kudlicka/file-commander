c2de8918cfe2549f491ae8743aad278649f52e15
isshou <37011117+martin-kudlicka@users.noreply.github.com>
2006-12-09 14:37:23 +0000

    + background operations working

--- FileOperation/CopyMoveWidget.h
+++ FileOperation/CopyMoveWidget.h
@@ -5,10 +5,28 @@
 
 #include "ui_CopyMoveWidget.h"
 
-class cCopyMoveWidget : public QWidget, public Ui::qwCopyMove
+class cCopyMoveWidget : public QWidget, private Ui::qwCopyMove
 {
+	Q_OBJECT
+
 	public:
 		cCopyMoveWidget(); ///< constructor
+
+	private slots:
+		void on_cCopyMove_SetCurrentMaximum(const qint64 qi64Value);
+																						///< set maximum for current file
+																						/**< \param qi64Value maximum for current file */
+		void on_cCopyMove_SetCurrentValue(const qint64 qi64Value);	///< set progress for current file
+																						/**< \param qi64Value progress for current file */
+		void on_cCopyMove_SetDestination(const QString qsDestination);
+																						///< set destination file
+																						/**< \param qsDestination destination file */
+		void on_cCopyMove_SetSource(const QString qsSource);			///< set source file
+																						/**< \param qsSource source file */
+		void on_cCopyMove_SetTotalMaximum(const qint64 qi64Value);	///< set overall maximum
+																						/**< \param qi64Value overall maximum */
+		void on_cCopyMove_SetTotalValue(const qint64 qi64Value);		///< set overall progress
+																						/**< \param qi64Value overall progress */
 }; // cCopyMoveWidget
 
 #endif
