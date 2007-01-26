72567722d52d907e248ef4b4188871a4c1172b11
isshou <37011117+martin-kudlicka@users.noreply.github.com>
2007-01-26 15:43:58 +0000

    + memorize settings when show Options dialog + restore all settings on cancel + add new column in column set + explicit option save for columns + set drive for default tab

--- OptionsDialog.h
+++ OptionsDialog.h
@@ -26,10 +26,25 @@ class cOptionsDialog : public QDialog, private Ui::qdOptions
 		static const int iCOLUMN_SETS_PAGE = 1;							///< column's sets page
 		static const int iPLUGINS_PAGE = 2;									///< main plugin's page
 		static const int iCONTENT_PLUGINS_PAGE = 3;						///< content plugin's page
+
+		static const int iPLUGIN_NAME_COLUMN = 0;							///< name of plugin
+		static const int iPLUGIN_ENABLED_COLUMN = 1;						///< plugin enabled / disabled
+
+		static const int iTYPE_COLUMN = 0;									///< type of column (native / plugin name)
+		static const int iNAME_COLUMN = 1;									///< internal name of column
+		static const int iUNIT_COLUMN = 2;									///< column's unit
+		static const int iSHOW_COLUMN = 3;									///< user's name of column
+
 		static const int iROW_SPACE = 5;										///< addition space in table rows
+
+		/// specific options to save
+		enum eOption {
+			Columns																	///< columns from column set
+		};
 
 		cContent *ccContent;														///< content plugins
 		cSettings *csSettings;													///< application's settings
+		QMap <QString, QString> qlOldOptions;								///< original application's settings
 		QMenu qmColumns;															///< columns menu
 		QMenu *qmNative;															///< native part of columns menu
 		QMenu *qmPlugins;															///< plugins part of columns menu
@@ -45,6 +60,8 @@ class cOptionsDialog : public QDialog, private Ui::qdOptions
 																							  \param qtwTree tree to fill */
 		void FillOptions();														///< fill options with set settings
 		void PrepareColumnsMenu();												///< prepare context columns menu
+		void SaveOption(const eOption &eoType);							///< save specific changes into settings file
+																						/**< \param eoType type of changes */
 		void SaveOptions();														///< save changes into application's settings file
 
 	private slots:
@@ -52,6 +69,9 @@ class cOptionsDialog : public QDialog, private Ui::qdOptions
 																						///< column set changed
 																						/**< \param text new selected column set */
 		void on_qdbbRespond_accepted();										///< changes accepted
+		void on_qdbbRespond_rejected();										///< changes rejected
+		void on_qmColumns_triggered(QAction *action);					///< column selected into column set
+																						/**< \param action selected column (or column's unit) */
 		void on_qpbAddContentPlugin_clicked(bool checked = false);
 																						///< add button is clicked on in content plugins
 																						/**< \param checked true if button is checkable and checked */
