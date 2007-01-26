f920890dd376ae0a04d625d4fa3bd36e005bc1dd
isshou <isshou@0a678794-6b23-0410-9a20-d9f6a9df69ad>
2007-01-26 15:39:31 +0000

    * in Options class content plugin list changed from QTableWidget to QTreeWidget + add and remove content plugin (changes not saved yet)

--- trunk/OptionsDialog.h
+++ trunk/OptionsDialog.h
@@ -22,18 +22,29 @@ class cOptionsDialog : public QDialog, private Ui::qdOptions
 
 		cSettings *csSettings;													///< application's settings
 
-		void BuildTree();															///< build settings tree with choices
-		void FillPluginsTable(const QList<cSettings::sPlugin> &qlPlugins, QTableWidget *qtwTable);
-																						///< fills plugin information into table
+		void AddPluginIntoTree(const cSettings::sPlugin &spPlugin, QTreeWidget *qtwTree);
+																						///< add another plugin into tree
+																						/**< \param spPlugin plugin to add
+																							  \param qtwTree tree to put plugin into */
+		void CreateChoices();													///< build choices tree
+		void FillPluginsTree(const QList<cSettings::sPlugin> &qlPlugins, QTreeWidget *qtwTree);
+																						///< fills plugin information into tree
 																						/**< \param qlPlugins plugin list
-																							  \param qtwTable table to fill */
-		void FillSettings();														///< fill options with set settings
+																							  \param qtwTree tree to fill */
+		void FillOptions();														///< fill options with set settings
 
 	private slots:
+		void on_qpbAddContentPlugin_clicked(bool checked = false);
+																						///< add button is clicked on in content plugins
+																						/**< \param checked true if button is checkable and checked */
+		void on_qpbRemoveContentPlugin_clicked(bool checked = false);
+																						///< remove content plugin button is clicked on
+																						/**< \param checked true if button is checkable and checked */
 		void on_qtwChoices_currentItemChanged(QTreeWidgetItem *current, QTreeWidgetItem *previous);
 																						///< choice change
 																						/**< \param current new choice
 																							  \param previous last choice */
+		void on_qtwContentPlugins_itemSelectionChanged();				///< selected content plugin changed
 }; // cOptionsDialog
 
 #endif
