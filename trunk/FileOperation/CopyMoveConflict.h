086e425ef8b258cb74a75d88b19bb9f21385d0c7
isshou <isshou@0a678794-6b23-0410-9a20-d9f6a9df69ad>
2007-02-12 13:55:31 +0000

    + CopyMoveConflictDialog class for user response on copy/move conflicts + CopyMoveConflict class for handling CopyMoveConflictDialog class + show conflict dialog when copy/move on existing file (not working yet, just preparation)

---
+++ trunk/FileOperation/CopyMoveConflict.h
@@ -0,0 +1,33 @@
+/// copy/move conflict
+
+#ifndef COPY_MOVE_CONFLICT_H
+#define COPY_MOVE_CONFLICT_H
+
+#include <QMainWindow>
+#include "FileOperation/CopyMoveConflictDialog.h"
+#include <QFileInfo>
+
+class cCopyMoveConflict : public QObject
+{
+	Q_OBJECT
+
+	public:
+		cCopyMoveConflict(QMainWindow *qmwParent);	///< constructor
+																	/**< \param qmwParent parent window of conflict dialog */
+		~cCopyMoveConflict();								///< destructor
+
+	private:
+		cCopyMoveConflictDialog *ccmcdDialog;			///< conflict dialog
+
+	signals:
+		void Finished(const cCopyMoveConflictDialog::eChoice &ecResponse);
+																	///< dialog closed with user response
+																	/**< \param ecResponse user's response */
+	private slots:
+		void Show(const QFileInfo &qfiSource, const QFileInfo &qfiDestination);
+																	///< show conflict dialog
+																	/**< \param qfiSource source file information
+																		  \param qfiDestination destination file information */
+}; // cCopyMoveConflict
+
+#endif
