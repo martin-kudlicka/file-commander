3bde93453d4b9b6f9ebc2a95667edfa4adeb9e9d
isshou <37011117+martin-kudlicka@users.noreply.github.com>
2007-03-21 19:42:19 +0000

    + fill source directory to start search in Find files dialog + put path to selected drives to search in

--- FindFilesDialog.h
+++ FindFilesDialog.h
@@ -6,18 +6,23 @@
 #include "ui_FindFilesDialog.h"
 
 #include <QMainWindow>
+#include "Panel.h"
 
 class cFindFilesDialog : public QDialog, private Ui::qdFindFiles
 {
 	Q_OBJECT
 
 	public:
-		cFindFilesDialog(QMainWindow *qmwParent);				///< constructor
-																			/**< \param qmwParent parent window of this dialog */																				/**< \param checked true if button is checkable and checked */
+		cFindFilesDialog(QMainWindow *qmwParent, cPanel *cpPanel);	///< constructor
+																						/**< \param qmwParent parent window of this dialog
+																							  \param cpPanel panel to work with */
+
+	private:
+		cPanel *cpPanel;															///< panel to work with
 
 	private slots:
-		void on_qpbDrives_clicked(bool checked = false);	///< drives button is clicked on
-																			/**< \param checked true if button is checkable and checked */
+		void on_qpbDrives_clicked(bool checked = false);				///< drives button is clicked on
+																						/**< \param checked true if button is checkable and checked */
 }; // cFindFilesDialog
 
 #endif
