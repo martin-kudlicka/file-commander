8e6d3772628e91ff24b210c9d5b7c5947c4649f2
isshou <isshou@0a678794-6b23-0410-9a20-d9f6a9df69ad>
2007-05-05 18:11:27 +0000

    + (Windows) simple command line processing to run programs with parameters

--- trunk/Panel/Process.cpp
+++ trunk/Panel/Process.cpp
@@ -19,7 +19,17 @@ void cProcess::Execute(const QString &qsProcess, const QString &qsPath /* NULL *
 		ShellExecute(NULL, NULL, reinterpret_cast<LPCWSTR>(qdDir.path().unicode()), NULL, NULL, SW_SHOWNORMAL);
 	} else {
 		// open file
-		ShellExecute(NULL, NULL, reinterpret_cast<LPCWSTR>(qsProcess.unicode()), NULL, reinterpret_cast<LPCWSTR>(qsPath.unicode()), SW_SHOWNORMAL);
+		QString qsName, qsParameters;
+
+		if (qsProcess.at(0) != '"') {
+			qsName = qsProcess.left(qsProcess.indexOf(' '));
+			qsParameters = qsProcess.mid(qsProcess.indexOf(' ') + 1);
+		} else {
+			qsName = qsProcess.mid(1, qsProcess.indexOf('"', 2) - 1);
+			qsParameters = qsProcess.mid(qsProcess.indexOf('"', 2) + 2);
+		} // if else
+
+		ShellExecute(NULL, NULL, reinterpret_cast<LPCWSTR>(qsName.unicode()), reinterpret_cast<LPCWSTR>(qsParameters.unicode()), reinterpret_cast<LPCWSTR>(qsPath.unicode()), SW_SHOWNORMAL);
 	} // if else
 #else
 	QProcess::startDetached(qsProcess);
