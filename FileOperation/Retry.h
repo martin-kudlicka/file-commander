6c9ad7755a6efe32f4c99377cb904c9877e918d4
isshou <37011117+martin-kudlicka@users.noreply.github.com>
2007-05-05 18:08:23 +0000

    + Retry dialog for copy/move operation - unused parent window parameter in Retry dialog

--- FileOperation/Retry.h
+++ FileOperation/Retry.h
@@ -22,10 +22,9 @@ class cRetry : public QObject
 		void Finished(const cRetryDialog::eChoice &ecResponse);	///< dialog closed with user response
 																					/**< \param ecResponse dialog result */
 	private slots:
-		void Show(QWidget *qwParent, const QString &qsInformation, const QString &qsFilename);
+		void Show(const QString &qsInformation, const QString &qsFilename);
 																					///< show retry dialog
-																					/**< \param qsParent parent window
-																						  \param qsInformation question about file
+																					/**< \param qsInformation question about file
 																						  \param qsFilename concerned file */
 }; // cRename
 
