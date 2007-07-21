2a3285cb53dce0a8b32d841bf5bb20a0db08866f
isshou <isshou@0a678794-6b23-0410-9a20-d9f6a9df69ad>
2007-07-21 11:01:37 +0000

    + executing local files

--- trunk/FileCommander/FileControl/Process.h
+++ trunk/FileCommander/FileControl/Process.h
@@ -1,4 +1,4 @@
-/// process handling
+/// process execution handling
 
 #ifndef PROCESS_H
 #define PROCESS_H
@@ -6,15 +6,17 @@
 #include <QtCore/QString>
 #include <QtCore/QObject>
 
-class cProcess : public QObject
+class cProcess : private QObject
 {
 	public:
-		static void Execute(const QString &qsProcess, const QString &qsPath = NULL);	///< execute file
-																												/**< \param qsProcess file to execute */
+		static void StartDetached(const QString &qsProcess, const QString &qsPath = NULL);	///< execute file
+																														/**< \param qsProcess file to execute
+																															  \param qsPath path to run file from */
 #ifdef Q_WS_WIN
+	private:
 		static QString GetErrorString(const int &iErrorCode);											///< error string for specified error code
-																												/**< \param iErrorCode error code
-																													  \return error string */
+																														/**< \param iErrorCode error code
+																															  \return error string */
 #endif
 }; // cProcess
 
